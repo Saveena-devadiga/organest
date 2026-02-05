8fd26cc46b8d367a9b7e1d96beea5d9331336301
Saveena-devadiga <saveenadevadiga8@gmail.com>
2026-02-05 22:11:03 +0530

    Secure: Use environment variables for DB connection

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,20 @@
-// backend/db.js
+require('dotenv').config(); // Load the .env file
 const mysql = require("mysql2");
 
-// MySQL database configuration
 const db = mysql.createConnection({
-  host: "localhost",     // MySQL host
-  user: "root",          // MySQL username (default in XAMPP is 'root')
-  password: "",  // Replace with your password
-  database: "organest_db",  // The name of your database
-  port: 3306             // Add this line to specify the custom port
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL!");
+    if (err) {
+        console.error("Error connecting to MySQL:", err);
+        return;
+    }
+    console.log("Connected to MySQL!");
 });
 
 module.exports = db;
